ab19ec61fe9b2c185135dfa421e8c1fdf66655c2
victor-021 <30280491+victor-021@users.noreply.github.com>
2022-03-28 15:55:04 +0200

    Add support for connection to database
    This includes adding support for database config parameters in both go
    and the docker-compose config.

--- go.mod
+++ go.mod
@@ -9,6 +9,18 @@ require (
 )
 
 require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/jackc/puddle v1.2.1 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.13.0 // indirect
@@ -16,6 +28,7 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/jackc/pgx/v4 v4.15.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -25,7 +38,6 @@ require (
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -1,28 +1,99 @@
 package main
 
 import (
-	// "log"
+	"context"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/joho/godotenv"
 )
+
+var (
+	dbpool       *pgxpool.Pool
+	server_url   string
+	database_url string
+)
+
+func setupConfig() {
+	// Load environment variables from .env file
+	godotenv.Load()
+
+	server_host := os.Getenv("SERVER_HOST")
+	server_port := os.Getenv("SERVER_PORT")
+	database_host := os.Getenv("DATABASE_HOST")
+	database_port := os.Getenv("DATABASE_PORT")
+	database_name := os.Getenv("POSTGRES_DB")
+	database_user := os.Getenv("POSTGRES_USER")
+	database_password := os.Getenv("POSTGRES_PASSWORD")
+
+	// Change empty config values to default values
+	if server_host == "" {
+		server_host = "localhost"
+	}
+	if server_port == "" {
+		server_port = "8081"
+	}
+	if database_host == "" {
+		database_host = "localhost"
+	}
+	if database_port == "" {
+		database_port = "5432"
+	}
+	if database_name == "" {
+		database_name = "backend-db"
+	}
+	if database_user == "" {
+		database_user = "dbuser"
+	}
+	if database_password == "" {
+		database_password = "kandidat-backend"
+	}
+
+	server_url = server_host + ":" + server_port
+	database_url = "postgres://" + database_user + ":" + database_password + "@" + database_host + ":" + database_port + "/" + database_name
+}
+
+func setupDBPool() *pgxpool.Pool {
+	dbpool, err := pgxpool.Connect(context.Background(), database_url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	return dbpool
+}
 
 func ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 
 func setupRouter() *gin.Engine {
+	gin.SetMode(os.Getenv("GIN_MODE"))
 	router := gin.Default()
 	router.GET("/ping", ping)
 	return router
 }
 
+func testDB() {
+	var greeting string
+	err := dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
+}
+
 func main() {
-	// Load environment variables from .env file
-	godotenv.Load()
+	setupConfig()
+	dbpool = setupDBPool()
+	defer dbpool.Close()
+
+	testDB()
 
 	router := setupRouter()
-	router.Run(os.Getenv("SERVER_URL"))
+	router.Run(server_url)
 }
